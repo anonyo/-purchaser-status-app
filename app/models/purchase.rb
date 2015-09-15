9865c06c6ba088b079f4772fd62b3190225548e6
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-15 09:24:42 -0400

    Replace define_method with more expressive has_statuses method

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,11 +1,3 @@
 class Purchase < ActiveRecord::Base
-  STATUSES = %w(pending in_progress submitted shipped received)
-
-  class << self
-    STATUSES.each do |status_name|
-      define_method "all_#{status_name}" do
-        where(status: status_name)
-      end
-    end
-  end
+  has_statuses :pending, :in_progress, :submitted, :shipped, :received
 end
